94617b328ef2bd8aeb07c4abdfe4cad3e41ff1d0
Alejandro Coronado <alejandro.coronado.n@gmail.com>
2021-02-23 10:48:35 -0600

    Assignment 1.1: First Delivery

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Media} from 'reactstrap';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
-
 class Dishdetail extends Component{
   constructor(props) {
     super(props);
@@ -10,31 +9,41 @@ class Dishdetail extends Component{
     }
   }
 
+  renderComments(comments){
+    const menucomment =comments.map((comment) => {
+      return (
+        <li class="list-unstyled">
+          <p> {comment.comment}</p>
+          <p>-- {comment.author}, {comment.date}</p>
+
+        </li>        
+
+        );
+      });
+
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <b>Comments</b>
+        <ul className="list-unstyled">
+          {menucomment}
+        </ul>
+      </div>
+      );
+    
+  }
 
   renderDish(dish){
     if (dish != null){
       return(
         <div className='row'>
-          <Card className="col-12 col-md-5 m-1">
+           <Card className="col-12 col-md-5 m-1">
             <CardImg widht='100%' src={dish.image} alt={dish.name}/>
             <CardBody>
               <CardTitle>{dish.name}</CardTitle>
               <CardText>{dish.description}</CardText>
             </CardBody>
           </Card>
-
-
-
-
-
-          
-          <Card className="col-12 col-md-5 m-1">
-            <CardImg widht='100%' src={dish.image} alt={dish.name}/>
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
+          {this.renderComments(dish.comments)}
       </div>
       );
     }
@@ -52,13 +61,9 @@ class Dishdetail extends Component{
         <div class="row">
             {this.props.menu}
         </div>
-        <div className="row">
           {this.renderDish(this.props.selectedDish)}
-        </div>
       </div>
     );
-
-  
   }
 };
 
