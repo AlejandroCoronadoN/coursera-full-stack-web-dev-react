e47df4f27d95fe909fb009a5c7b2ab1663cf2fe9
Alejandro Coronado <alejandro.coronado.n@gmail.com>
2021-02-26 11:28:06 -0600

    Functional components, information is only passed with props and the components are executed an returned as fucntions. Functions inside funcations to render using information only

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,70 +1,60 @@
-import React, {Component} from 'react';
-import {Media} from 'reactstrap';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
-class Dishdetail extends Component{
-  constructor(props) {
-    super(props);
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-    this.state = {
-    }
-  }
-
-  renderComments(comments){
-    const menucomment =comments.map((comment) => {
+function RenderComments({ comments }) {
+    console.log("comments: ", comments);
+    const menucomment = comments.map((comment) => {
       return (
         <li class="list-unstyled">
           <p> {comment.comment}</p>
-          <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', dat:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-
-        </li>        
-
-        );
-      });
+          <p>
+            -- {comment.author},{" "}
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              dat: "2-digit",
+            }).format(new Date(Date.parse(comment.date)))}
+          </p>
+        </li>
+      );
+    });
 
     return (
-      <div className="col-12 col-md-5 m-1">
+      <div className="col-12 col-md-5">
         <b>Comments</b>
-        <ul className="list-unstyled">
-          {menucomment}
-        </ul>
-      </div>
-      );
-    
-  }
-
-  renderDish(dish){
-    console.log('DISH renderDish: ', dish);
-    if (dish != null){
-      return(
-        <div className='row'>
-           <Card className="col-12 col-md-5 m-1">
-            <CardImg widht='100%' src={dish.image} alt={dish.name}/>
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-          {this.renderComments(dish.comments)}
-      </div>
-      );
-    }
-    else{
-      return(
-        <div> </div>
-      );
-    }
-  }
-
-  render(){
-
-    return(
-      <div class="container">
-        <div class="row">
-            {this.props.menu}
-        </div>
-          {this.renderDish(this.props.dish)}
+        <ul className="list-unstyled">{menucomment}</ul>
       </div>
     );
+
+}
+
+function RenderDish({ dish }) {
+  console.log("DISH renderDish: ", dish);
+ 
+    return (
+        <Card className="col-12 col-md-5">
+          <CardImg widht="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+    );
+
+}
+
+const Dishdetail = (props) => {
+  if (props.dish != null) {
+  return (
+    <div class="container">
+      <div className="row">
+        <RenderDish dish={props.dish} />
+        <RenderComments comments={props.dish.comments} />
+      </div>
+    </div>
+  );
+  }else{
+    return (<div></div>);
   }
 };
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,28 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card, CardImg, CardImgOverlay,CardTitle} from 'reactstrap';
-import Dishdetail from './DishdetailComponent';
-class Menu extends Component{
 
-  constructor(props) {
-    super(props);
+  function RenderMenuItem({dish, onClick}){ //? Specify props individually it could also be props
+    return(
+      <Card onClick={()=> onClick(dish.id) }> 
+      <CardImg widht='100%' src={dish.image} alt={dish.name}/>
+      <CardImgOverlay body className="ml-5">
+        <CardTitle heading>{dish.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>    );
   }
-
-  
   //? Whenever you create a list of items in react each item requires an ID
   //? Each Media REACT item will work as a lielement (elementy from list) this is how it works by documentation
   //? Noite that this Media list component continues inside the definition of menu that contains Media body Media li etc...
   //? Stores properties related to this component that we can use
 
-  render(){
-    const menu= this.props.dishes.map((dish) => {
+  const Menu = (props) => {
+    const menu= props.dishes.map((dish) => { //? Note that we dont need any longer this.props since there is no object in thes component
       return (
-        <div key={dish.id} class="col-12 col-md-5 m-1">
-          <Card onClick={()=> this.props.onClick(dish.id) }> 
-            <CardImg widht='100%' src={dish.image} alt={dish.name}/>
-            <CardImgOverlay body className="ml-5">
-              <CardTitle heading>{dish.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
+        <div key={dish.id} class="col-12 col-md-5">
+            <RenderMenuItem dish={dish} onClick={props.onClick}></RenderMenuItem>
         </div>
       );
     });
@@ -36,7 +33,6 @@ class Menu extends Component{
     );
 
   }
-};
 
 export default Menu;
 
